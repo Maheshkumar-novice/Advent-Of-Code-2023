495cf7a0a5b1bc92580908dba6edebab782ea512
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2023-12-11 20:02:53 +0530

    Refactor: Day-10

--- Day-10/part-1.py
+++ Day-10/part-1.py
@@ -1,3 +1,4 @@
+import re
 from collections import deque
 
 PIPE_MATCH_MAP = {
@@ -40,126 +41,86 @@ PIPE_MATCH_MAP = {
 }
 
 
-def get_connections(coords, data):
-    r_max = len(data[0])
-    b_max = len(data)
-
-
+def get_connections(coords, data, row_length, column_length):
     top = (coords[0] - 1, coords[1])
     bottom = (coords[0] + 1, coords[1])
     right = (coords[0], coords[1] + 1)
     left = (coords[0], coords[1] - 1)
 
-
-    if not validate_top(top):
+    if not (top[0] >= 0 and top[1] >= 0):
         top = None
 
-    if not validate_bottom(bottom, b_max):
+    if not (bottom[0] >=0 and bottom[1] >= 0 and bottom[0] < column_length):
         bottom = None
 
-    if not validate_right(right, r_max):
+    if not (right[0] >= 0 and right[1] >= 0 and right[1] < row_length):
         right = None
 
-    if not validate_left(left):
+    if not (left[0] >= 0 and left[1] >= 0):
         left = None
 
-    return top, bottom, right, left
-
-def validate_top(top): return top[0] >= 0 and top[1] >= 0
-
-def validate_bottom(bottom, b_max): return bottom[0] >=0 and bottom[1] >= 0 and bottom[0] < b_max
-
-def validate_right(right, r_max): return right[0] >= 0 and right[1] >= 0 and right[1] < r_max
-
-def validate_left(left): return left[0] >= 0 and left[1] >= 0
-
-def has_connection(coords, data, connections=[], count=False):
-    top, bottom, right, left = get_connections(coords, data) if not connections else connections
+    top_connectable, bottom_connectable, right_connectable, left_connectable = None, None, None, None
 
     symbol = data[coords[0]][coords[1]]
-    if symbol == '.' and count == False:
-        return None, None, None, None  
-    elif symbol == '.' and count:
-        return False
 
-    top_r = None
-    if top:
-        top_r = data[top[0]][top[1]] in PIPE_MATCH_MAP[symbol]['top']
+    if top and data[top[0]][top[1]] in PIPE_MATCH_MAP[symbol]['top']:
+        top_connectable = top
 
-    bottom_r = None
-    if bottom:
-        bottom_r = data[bottom[0]][bottom[1]] in PIPE_MATCH_MAP[symbol]['bottom']
+    if bottom and data[bottom[0]][bottom[1]] in PIPE_MATCH_MAP[symbol]['bottom']:
+        bottom_connectable = bottom
 
-    right_r = None
-    if right:
-        right_r = data[right[0]][right[1]] in PIPE_MATCH_MAP[symbol]['right']
+    if right and data[right[0]][right[1]] in PIPE_MATCH_MAP[symbol]['right']:
+        right_connectable = right
 
-    left_r = None
-    if left:
-        left_r = data[left[0]][left[1]] in PIPE_MATCH_MAP[symbol]['left']
+    if left and data[left[0]][left[1]] in PIPE_MATCH_MAP[symbol]['left']:
+        left_connectable = left
 
-   
-    counter = 0
-    for i in [top_r, bottom_r, right_r, left_r]:
-        if i:
-            counter += 1
+    return top_connectable, bottom_connectable, right_connectable, left_connectable
 
-    if count:
-        return counter == 2
-    else:
-        if counter != 2:
-            return None, None, None, None
-        val = []
-        if top_r:
-            val.append(top)
-        if bottom_r:
-            val.append(bottom)
-        if right_r:
-            val.append(right)
-        if left_r:
-            val.append(left)
-        return val
+
 with open('input.txt', 'r') as f:
-    import re
-    fc = f.read()
-    rei = re.search(r'S', fc)
-    s_index = rei.span()[0]
+    file_contents = f.read()
 
-    fcl = fc.splitlines()
-    rl = len(fcl[0])
-    cl = len(fcl)
+    s_place = re.search(r'S', file_contents)
+    s_index_start = s_place.span()[0]
 
-    data = fc.splitlines()
+    data = file_contents.splitlines()
+    row_length = len(data[0])
+    column_length = len(data)
+
+    s_index = (s_index_start // row_length, s_index_start - ((s_index_start // row_length) * (row_length + 1)))
 
     for idx, i in enumerate(data):
        data[idx] = list(i)
-    
-    s_index = (s_index // rl,s_index - ((s_index// rl) * (rl + 1)))
 
     for i in ['|', '-', 'F', '7', 'J', 'L']:
         data[s_index[0]][s_index[1]] = i
-        r = has_connection(s_index, data, count=True)
 
-        if r:
+        counter = 0
+        for i in get_connections(s_index, data, row_length, column_length):
+            if i:
+                counter += 1
+
+        if counter == 2:
             break
 
     queue = deque()
     queue.append(s_index)
-    visited = {}
-    parent_count = {s_index: 0}
-    while len(queue) != 0:
+
+    main_loop = set()
+    count_map = {s_index: 0}
+
+    while queue:
         pipe = queue.popleft()
 
-        if pipe in visited:
+        if (pipe in main_loop) or (data[pipe[0]][pipe[1]] == '.'):
             continue
 
-        if data[pipe[0]][pipe[1]] == '.':
-            continue
-        
-        connections = get_connections(pipe, data)
-        for i in has_connection(pipe, data, connections=connections):
-            if i and i not in visited:
+        for i in get_connections(pipe, data, row_length, column_length):
+            if i and i not in main_loop:
                 queue.append(i)
-                parent_count[i] = parent_count[pipe] + 1
-        visited[pipe] = True
-    print(max(parent_count.values()))
+                count_map[i] = count_map[pipe] + 1
+
+        main_loop.add(pipe)
+
+    print(max(count_map.values()))
--- Day-10/part-2.py
+++ Day-10/part-2.py
@@ -1,3 +1,4 @@
+import re
 from collections import deque
 
 PIPE_MATCH_MAP = {
@@ -40,147 +41,99 @@ PIPE_MATCH_MAP = {
 }
 
 
-def validate_top(top): return top[0] >= 0 and top[1] >= 0
-
-def validate_bottom(bottom, b_max): return bottom[0] >=0 and bottom[1] >= 0 and bottom[0] < b_max
-
-def validate_right(right, r_max): return right[0] >= 0 and right[1] >= 0 and right[1] < r_max
-
-def validate_left(left): return left[0] >= 0 and left[1] >= 0
-
-def get_connections(coords, data):
-    r_max = len(data[0])
-    b_max = len(data)
-
-
+def get_connections(coords, data, row_length, column_length):
     top = (coords[0] - 1, coords[1])
     bottom = (coords[0] + 1, coords[1])
     right = (coords[0], coords[1] + 1)
     left = (coords[0], coords[1] - 1)
 
-
-    if not validate_top(top):
+    if not (top[0] >= 0 and top[1] >= 0):
         top = None
 
-    if not validate_bottom(bottom, b_max):
+    if not (bottom[0] >=0 and bottom[1] >= 0 and bottom[0] < column_length):
         bottom = None
 
-    if not validate_right(right, r_max):
+    if not (right[0] >= 0 and right[1] >= 0 and right[1] < row_length):
         right = None
 
-    if not validate_left(left):
+    if not (left[0] >= 0 and left[1] >= 0):
         left = None
 
-    return top, bottom, right, left
-
-
-
-def has_connection(coords, data, connections=[], count=False):
-    top, bottom, right, left = get_connections(coords, data) if not connections else connections
+    top_connectable, bottom_connectable, right_connectable, left_connectable = None, None, None, None
 
     symbol = data[coords[0]][coords[1]]
-    if symbol == '.' or symbol == 'O' and count == False:
-        return None, None, None, None  
-    elif symbol == '.' and count:
-        return False
 
-    top_r = None
-    if top:
-        top_r = data[top[0]][top[1]] in PIPE_MATCH_MAP[symbol]['top']
+    if top and data[top[0]][top[1]] in PIPE_MATCH_MAP[symbol]['top']:
+        top_connectable = top
 
-    bottom_r = None
-    if bottom:
-        bottom_r = data[bottom[0]][bottom[1]] in PIPE_MATCH_MAP[symbol]['bottom']
+    if bottom and data[bottom[0]][bottom[1]] in PIPE_MATCH_MAP[symbol]['bottom']:
+        bottom_connectable = bottom
 
-    right_r = None
-    if right:
-        right_r = data[right[0]][right[1]] in PIPE_MATCH_MAP[symbol]['right']
+    if right and data[right[0]][right[1]] in PIPE_MATCH_MAP[symbol]['right']:
+        right_connectable = right
 
-    left_r = None
-    if left:
-        left_r = data[left[0]][left[1]] in PIPE_MATCH_MAP[symbol]['left']
+    if left and data[left[0]][left[1]] in PIPE_MATCH_MAP[symbol]['left']:
+        left_connectable = left
 
-   
-    counter = 0
-    for i in [top_r, bottom_r, right_r, left_r]:
-        if i:
-            counter += 1
-
-    if count:
-        return counter == 2
-    else:
-        if counter != 2:
-            return None, None, None, None
-        val = []
-        if top_r:
-            val.append(top)
-        if bottom_r:
-            val.append(bottom)
-        if right_r:
-            val.append(right)
-        if left_r:
-            val.append(left)
-        return val
+    return top_connectable, bottom_connectable, right_connectable, left_connectable
 
 
 with open('input.txt', 'r') as f:
-    import re
-    fc = f.read()
-    rei = re.search(r'S', fc)
-    s_index = rei.span()[0]
+    file_contents = f.read()
 
-    fcl = fc.splitlines()
-    rl = len(fcl[0])
-    cl = len(fcl)
+    s_place = re.search(r'S', file_contents)
+    s_index_start = s_place.span()[0]
 
-    s_index = (s_index // rl,s_index - ((s_index// rl) * (rl + 1)))
+    data = file_contents.splitlines()
+    row_length = len(data[0])
+    column_length = len(data)
 
-    data = fc.splitlines()
+    s_index = (s_index_start // row_length, s_index_start - ((s_index_start // row_length) * (row_length + 1)))
 
     for idx, i in enumerate(data):
        data[idx] = list(i)
-    
-    s_index = (24, 93)
 
     for i in ['|', '-', 'F', '7', 'J', 'L']:
         data[s_index[0]][s_index[1]] = i
-        r = has_connection(s_index, data, count=True)
 
-        if r:
+        counter = 0
+        for i in get_connections(s_index, data, row_length, column_length):
+            if i:
+                counter += 1
+
+        if counter == 2:
             break
 
     queue = deque()
     queue.append(s_index)
-    visited = {}
-    parent_count = {s_index: 0}
-    while len(queue) != 0:
+
+    main_loop = set()
+
+    while queue:
         pipe = queue.popleft()
 
-        if pipe in visited:
+        if (pipe in main_loop) or (data[pipe[0]][pipe[1]] == '.'):
             continue
 
-        if data[pipe[0]][pipe[1]] == '.':
-            continue
-        
-        connections = get_connections(pipe, data)
-        
-        for i in has_connection(pipe, data, connections=connections):
-            if i and i not in visited:
+        for i in get_connections(pipe, data, row_length, column_length):
+            if i and i not in main_loop:
                 queue.append(i)
-                parent_count[i] = parent_count[pipe] + 1
-        visited[pipe] = True
 
-    total = 0
+        main_loop.add(pipe)
+
+    total_insiders = 0
     for idx, i in enumerate(data):
         inside = False
-        wall = None
+        wall_start = None
         for jdx, j in enumerate(i):
-            if (idx, jdx) in visited:
+            if (idx, jdx) in main_loop:
                 pipe = data[idx][jdx]
-                if pipe == '|' or (wall == 'F' and pipe == 'J') or (wall == 'L' and pipe == '7'):
+                # Can go with Regex/Replace but To find `S` it was converted to a list.
+                if pipe == '|' or (wall_start == 'F' and pipe == 'J') or (wall_start == 'L' and pipe == '7'):
                     inside = not inside
                 elif pipe  in 'FL':
-                    wall = pipe
+                    wall_start = pipe
             elif inside:
-                total += 1
-    print(total)
+                total_insiders += 1
+
+    print(total_insiders)

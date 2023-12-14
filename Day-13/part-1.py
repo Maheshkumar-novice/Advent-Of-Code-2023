5bbe40f556cf4f4c3ea21742ebfda2fb9b836537
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2023-12-14 10:34:03 +0530

    Refactor: Day-13/Part-1

--- Day-13/part-1.py
+++ Day-13/part-1.py
@@ -1,6 +1,3 @@
-import numpy
-
-
 def get_count(data):
     for intersection_index in range(1, len(data)):
         taking = data[:intersection_index][::-1]
@@ -13,19 +10,12 @@ def get_count(data):
 with open('input.txt', 'r') as f:
     total = 0
     for line in f.read().split('\n\n'):
-        r_data = line.split()
+        data = line.split()
 
-        if row_wise := get_count(r_data):
+        if row_wise := get_count(data):
             total += row_wise * 100
 
-        d = []
-        for line in r_data:
-            d.append(list(line))
-        c_data = numpy.transpose(d).tolist()
-        c_data = [''.join(line) for line in c_data]
-        column_wise = get_count(c_data)
-
-        if column_wise := get_count(c_data):
+        if column_wise := get_count([*zip(*data)]):
             total += column_wise
 
       

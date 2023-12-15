a2dd3cbf9475ed20e19a1553d9ee07c7cbbe8019
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2023-12-15 13:34:06 +0530

    Refactor: Day-15/Part-2

--- Day-15/part-2.py
+++ Day-15/part-2.py
@@ -1,31 +1,29 @@
 from collections import defaultdict
 
+
+def hash(label):
+    label_hash = 0
+    for char in label:
+        label_hash += ord(char)
+        label_hash *= 17
+        label_hash %= 256
+
+    return label_hash
+
+
 with open('input.txt', 'r') as f:
-    total, boxes, focal_length_map = 0, defaultdict(list), defaultdict(int)
-    
+    total, boxes = 0, defaultdict(dict)
+
     for seq in f.read().strip().split(','):
         if '-' in seq:
-            label, operation = seq.split('-')[0], '-'
+            label = seq[:-1]
+            boxes[hash(label)].pop(label, None)
         else:
             label, focal_length = seq.split('=')
-            focal_length_map[label], operation = int(focal_length), '='
-
-        label_hash = 0
-        for char in label:
-            label_hash += ord(char)
-            label_hash *= 17
-            label_hash %= 256
-        
-        match operation:
-            case '=':
-                if label not in boxes[label_hash]:
-                    boxes[label_hash].append(label)
-            case '-':
-                if label in boxes[label_hash]:
-                    boxes[label_hash].remove(label)
+            boxes[hash(label)][label] = int(focal_length)
 
     for box, lenses in boxes.items():
-        for idx, lens in enumerate(lenses):
-            total += (box + 1) * (idx + 1) * focal_length_map[lens]
+        for idx, focal_length in enumerate(lenses.values()):
+            total += (box + 1) * (idx + 1) * focal_length
         
     print(total)

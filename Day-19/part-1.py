dfb0f1ba9bd9795efc756f98fd1eaef2544c7993
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2023-12-19 15:41:44 +0530

    Refactor: Day-19/Part-1

--- Day-19/part-1.py
+++ Day-19/part-1.py
@@ -1,56 +1,36 @@
-import re
 import operator
+import re
 
 with open('input.txt', 'r') as f:
-    operator_map = {
-        '>': operator.gt,
-        '<': operator.lt
-    }
-    instructions = {}
+    operator_map, instructions, total = {'>': operator.gt, '<': operator.lt}, {}, 0
+
+    def exec_instruction(instruction='in'):
+        if instruction in 'RA':
+            return 'RA'.index(instruction)
+
+        steps = iter(instructions[instruction])
+        while True:
+            match next(steps).split(':'):
+                case [condition, next_instruction]:
+                    if operator_map[condition[1]](line_val_map[condition[0]], int(condition[2:])):
+                        return exec_instruction(next_instruction)
+                case [next_instruction]:
+                    return exec_instruction(next_instruction)
+
     for line in f:
         if line == '\n':
             break
 
-        matches = re.match(r'(\w+){((\w+[><]\d+:\w+,)+(\w+))}', line).groups()
-        key, instruction, *_ = matches
-        instruction = instruction.split(',')
-        instructions[key] = instruction
-    
-    def exec_instruction(line_val_map, instruction='in'):
-        steps = iter(instructions[instruction])
-        while True:
-            step = next(steps).split(':')
-            match step:
-                case condition, instruction:
-                    key = line_val_map[condition[0]]
-                    op = operator_map[condition[1]]
-                    value = int(condition[2:])
-                    if op(key, value):
-                        if instruction == 'A':
-                            return 1
-                        elif instruction == 'R':
-                            return 0
-                        else:
-                            return exec_instruction(line_val_map, instruction)
-                case instruction:
-                    instruction = instruction[0]
-                    if instruction == 'A':
-                        return 1
-                    elif instruction == 'R':
-                        return 0
-                    else:
-                        return exec_instruction(line_val_map, instruction)
+        key, instruction, *_  = re.match(r'(\w+){((\w+[><]\d+:\w+,)+(\w+))}', line).groups()
+        instructions[key] = instruction.split(',')
 
-    total = 0
     for line in f:
-        line = line.strip()[1:-1].split(',')
-        line_val_map = {}
+        line, line_val_map = line.strip()[1:-1].split(','), {}
         for val in line:
             a, b = val.split('=')
             line_val_map[a] = int(b)
 
-        is_accepted = exec_instruction(line_val_map)
-        if is_accepted:
+        if exec_instruction():
             total += sum(line_val_map.values())
 
     print(total)

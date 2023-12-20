38cdedaa1e04ad7233e435c037c760ff07c00345
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2023-12-20 12:15:02 +0530

    Refactor: Day-19/Part-2

--- Day-19/part-2.py
+++ Day-19/part-2.py
@@ -1,71 +1,50 @@
 import re
+from math import prod
 
-with open('input.txt', 'r') as f:
-    instructions, total, results = {}, 0, []
-    instructions['R'] = 'R'
-    instructions['A'] = 'A'
 
-    def exec_instruction(line_val_map, instruction): 
-        match instruction:
-            case 'A':
-                results.append(line_val_map)
-                return
-            case 'R':
-                return
-     
-        instruction = instruction[::-1]
-        step = instruction.pop().split(':')
+def exec_workflow(xmas_range_map, workflow, total=0): 
+        workflow = workflow[::-1]
 
-        match step:
-            case [condition, next_instruction]:
-                operator = condition[1]
-                line_val = condition[0]
-                combos = line_val_map[line_val]
-                value = int(condition[2:])
+        match workflow:
+            case ['A']:
+                return prod((xmas_val[-1] - xmas_val[0]) + 1 for xmas_val in xmas_range_map.values())
+            case ['R']:
+                return 0
+
+        match workflow.pop().split(':'):
+            case [condition, next_workflow]:
+                operator, xmas_val, value = condition[1], condition[0], int(condition[2:])
+                range = xmas_range_map[xmas_val]
 
                 if operator == '<':
-                    split_1_map = line_val_map.copy()
-                    split_1_combos = range(combos.start, value - 1)
-                    split_1_map[line_val] = split_1_combos
-                    exec_instruction(split_1_map, instructions[next_instruction])
+                    split_1_map = xmas_range_map.copy()
+                    split_1_map[xmas_val] = (range[0], value - 1)
+                    total += exec_workflow(split_1_map, workflows[next_workflow])
 
-                    split_2_map = line_val_map.copy()
-                    split_2_combos = range(value, combos.stop)
-                    split_2_map[line_val] = split_2_combos
-                    exec_instruction(split_2_map, instruction[::-1].copy())
+                    split_2_map = xmas_range_map.copy()
+                    split_2_map[xmas_val] = (value, range[-1])
+                    total += exec_workflow(split_2_map, workflow[::-1])
                 if operator == '>':
-                    split_1_map = line_val_map.copy()
-                    split_1_combos = range(value + 1, combos.stop)
-                    split_1_map[line_val] = split_1_combos
-                    exec_instruction(split_1_map, instructions[next_instruction])
+                    split_1_map = xmas_range_map.copy()
+                    split_1_map[xmas_val] = (value + 1, range[-1])
+                    total += exec_workflow(split_1_map, workflows[next_workflow])
 
-                    split_2_map = line_val_map.copy()
-                    split_2_combos = range(combos.start, value)
-                    split_2_map[line_val] = split_2_combos
-                    exec_instruction(split_2_map, instruction[::-1].copy())
-            case [next_instruction]:
-                exec_instruction(line_val_map, instructions[next_instruction])
+                    split_2_map = xmas_range_map.copy()
+                    split_2_map[xmas_val] = (range[0], value)
+                    total += exec_workflow(split_2_map, workflow[::-1])
+            case [next_workflow]:
+                total += exec_workflow(xmas_range_map, workflows[next_workflow])
+
+        return total
+
+with open('input.txt', 'r') as f:
+    workflows = {'R': ['R'], 'A': ['A']}
 
     for line in f:
         if line == '\n':
             break
 
-        key, instruction, *_  = re.match(r'(\w+){((\w+[><]\d+:\w+,)+(\w+))}', line).groups()
-        instructions[key] = instruction.split(',')
+        key, workflow, *_  = re.match(r'(\w+){((\w+[><]\d+:\w+,)+(\w+))}', line).groups()
+        workflows[key] = workflow.split(',')
 
-    line_val_map = {
-        'x': range(1, 4000),
-        'm': range(1, 4000),
-        'a': range(1, 4000),
-        's': range(1, 4000)
-    }
-    
-    exec_instruction(line_val_map, instructions['in'])
-
-    for result in results:
-        temp = 1
-        for line_val in result.values():
-            temp *= ((line_val.stop - line_val.start) + 1)
-        total += temp
-
-    print(total)
+    print(exec_workflow({i: (1, 4000) for i in 'xmas'}, workflows['in']))
